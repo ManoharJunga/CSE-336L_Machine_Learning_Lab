8b93b98a9dcfca7c5c4559e6f0b9d5b88efa924c
ManoharJunga <manohar_junga@srmap.edu.in>
2024-02-05 18:41:31 +0530

    week2

--- AP21110010957-Week-1/numpy/5.py
+++ AP21110010957-Week-1/numpy/5.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 # Create a 1-D array with 30 evenly spaced elements between 2.5 and 6.5
-my_array = np.linspace(2.5, 6.5, 30)
+my_array = np.linspace(0, 1, 10)
 
 # Print the array
 print("1-D array:", my_array)
